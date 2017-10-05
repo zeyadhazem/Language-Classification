b7b8c190427f4c42e406b63cf304a54985cbf30c
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-10-04 20:58:11 -0400

    Implemented TFIDF

--- base.py
+++ base.py
@@ -1,10 +1,15 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import re
+import math
+
+from matplotlib.ticker import FuncFormatter, MultipleLocator
+from collections import Counter
 
 d = pd.read_csv("train_set_x.csv")
 r = pd.read_csv("train_set_y.csv")
 
-# Merge the 2 dataframes in a single table by joining on Id
 data = pd.merge(d,r,how='inner',on='Id')
 data.drop("Id", axis=1, inplace=True)
 
@@ -41,26 +46,120 @@ def cleanup (s):
 data['Text'] = data['Text'].apply(cleanup) # Clean the data up!
 
 def charCount (s):
+    """
+    Takes in a unicode string representing a string
+    Returns a dict with letters as keys and their number of occurence as values
+    """
     d = {}
-
-    for c in s:
-        # Filter all numbers
-        if c > '0' and c < '9':
-            continue
-
-        lower = c.lower()
+    for each in s:
+        lower = each.lower()
 
         if lower in d.keys():
             d[lower] = d[lower] + 1
         else:
             d[lower] = 1
-    i = ''
 
-    if bool(d): #If not empty
-        i = max(d, key=d.get)
-
-    return i
+    return d
 
 data['MCL'] = data['Text'].apply(charCount)
 
-data.to_csv("out.csv", encoding="utf-8")
+d = {}
+data.count(numeric_only=True)
+length = int (data.count(numeric_only=True))
+
+# Count the frequency of the letters by language category
+for i in range(0, length - 1):
+
+    cat = str (data['Category'][i])
+    if cat in d.keys():
+        d[cat] = dict (Counter(d[cat]) + Counter(data['MCL'][i]))
+    else:
+        d[cat] = data['MCL'][i]
+
+def printbar (h, legend):
+    lists = sorted(h.items()) # sorted by key, return a list of tuples
+
+    x_ticks_labels, y = zip(*lists) # unpack a list of pairs into two tuples
+    x = np.array(np.arange(0,len(x_ticks_labels)))
+
+    plt.figure(figsize=(20,4))
+    plt.bar(np.arange(0,len(h)), h.values(), color='g')
+    plt.xticks(x, h.keys())
+    plt.legend(legend)
+    plt.show()
+
+def normalize (d):
+    """
+    Takes in a dictionary containing all the letters organized by language category
+    calculates the frequency of each letter by language category
+    Returns a dict containing the normalized freq of letters whose freq > uniform distribution prob
+    """
+    total_count = sum(d.values())
+    add = 0
+    h = d.copy()
+
+    for key in h.keys():
+        h[key] = float(h[key])/total_count
+        add = add + h[key]
+
+    print(add, len(h))
+    uniform = add/len(h) # if uniformly distributed
+    for key in h.keys():
+        # Truncate anything that has a rec value less than uniform
+        if (h[key] < uniform):
+            del h[key]
+
+    return h
+
+for key in d.keys():
+    h = dict(d[key]) # Validating and normalizing just by looking at each language on its own
+    h = normalize(h)
+    printbar(h, key)
+#     printbar(dict(d[key]), key)
+
+
+def docFrequency (d):
+
+    df = {}
+    for language in d:
+        for letter in d[language]:
+            if letter in df:
+                df[letter] = df[letter] + 1
+            else:
+                df[letter] = 1
+
+    return df
+
+
+def inverseDocFrequency (d):
+    """
+    Takes in a dictionary containing all the letters organized by language category
+    Returns a dictionary where the keys are the letters and the values are the idf
+    """
+    df = docFrequency(d)
+    length = float(len(d))
+
+    for letter in df:
+        df[letter] = math.log10(length/df[letter])
+
+    return df
+
+
+idf = inverseDocFrequency(d)
+
+# Now all we need to do is for every language, multiply the tf by idf
+tfidf = d.copy()
+for language in tfidf:
+    temp = tfidf[language]
+    for letter in temp:
+        temp[letter] = temp[letter] * idf[letter] # Will not throw an error since all letters are contained in idf
+
+
+for key in tfidf.keys():
+    h = dict(tfidf[key]) # Validating and normalizing just by looking at each language on its own
+    h = normalize(h)
+    printbar(h, key)
+    for letter in h:
+        print (letter)
+
+# data.to_csv("out.csv", encoding="utf-8")
